15275ec45139d5e1286228a38ceb5aed2c48b9ff
Co3code <countian3@gmail.com>
2026-01-25 22:39:36 +0800

    exercise 2-4

---
+++ exercise-2-4.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Temperature Converter</title>
+</head>
+<body>
+
+<h2>Exercise</h2>
+
+<form method="post">
+    Fahrenheit:
+    <input type="number" name="fahrenheit" step="any" required>
+
+    =
+    
+    Celsius:
+    <input type="text" value="<?php echo isset($celsius) ? $celsius : ''; ?>" readonly>
+
+    <br><br>
+
+    <button type="submit">Convert</button>
+</form>
+
+<?php
+if (isset($_POST['fahrenheit'])) {
+    $fahrenheit = $_POST['fahrenheit'];
+    $celsius = ($fahrenheit - 32) * 5 / 9;
+}
+?>
+
+</body>
+</html>
