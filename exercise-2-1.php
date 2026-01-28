7a6b275287571bf2209743f74e29afffa445e951
Co3code <countian3@gmail.com>
2026-01-28 23:46:04 +0800

    adding tittle and heading

--- exercise-2-1.php
+++ exercise-2-1.php
@@ -26,9 +26,10 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Slope Calculator</title>
   </head>
   <body>
+    <h2>Exercise 2-1</h2>
    <form action="" method="post">
     <label for="y2">enter a value for y2:</label><br>
     <input type="number" step="any" name="y2" required id="y2"><br>
@@ -39,7 +40,7 @@
     <input type="number" step="any" name="x2" required> <br>
 
     <label for="x1">enyer a value for x1</label><br>
-    <input type="number" step="any" name="x1" required><br>
+    <input type="number" step="any" name="x1" required><br><br>
 
     <input type="submit" name="submit" value="submit">
 
