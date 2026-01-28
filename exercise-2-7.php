31c575e5d4c2ec49580ae6ebc5c468bb76a4e024
Co3code <countian3@gmail.com>
2026-01-28 23:21:25 +0800

    exercise 2-7

--- exercise-2-7.php
+++ exercise-2-7.php
@@ -21,7 +21,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Increment / Decrement Counter</title>
 </head>
 <body>
     <h2>Exercise 2-7</h2>
