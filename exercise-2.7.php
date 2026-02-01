97421643640b7302ecb7003c6a2cb92b2316e445
Co3code <countian3@gmail.com>
2026-02-01 22:48:57 +0800

    exercise 2-7

--- exercise-2.7.php
+++ exercise-2.7.php
@@ -1,12 +1,51 @@
+<?php
+    $greet = "";
+
+    if (isset($_POST['submit'])) {
+    $language = $_POST['language'];
+
+    if ($language == "English") {
+        $greet = "Good Morning";
+    } elseif ($language == "Filipino") {
+        $greet = "Magandang Umaga";
+    } elseif ($language == "Chinese") {
+        $greet = "Ni Haw";
+    } elseif ($language == "Japanese") {
+        $greet = "Kinichi wa";
+    } else {
+        $greet = "Unkown Language";
+    }
+    }
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercise 2-7</title>
+    <title>Language Selector</title>
+
 </head>
 <body>
-    <h2></h2>
-    
+
+<form method="post">
+    select language:
+    <select name="language">
+        <option value="English">English</option>
+        <option value="Filipino">Filipino</option>
+        <option value="Chinese">Chinese</option>
+        <option value="Japanese">Japanese</option>
+    </select>
+
+    <button type="submit" name="submit" class="btn-green">
+        Greet
+    </button>
+</form>
+
+
+    <?php if ($greet != ""): ?>
+        <p><?php echo $greet ?></p>
+    <?php endif; ?>
+
+
+
 </body>
 </html>
