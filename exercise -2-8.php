eb322c94e048d09c98f1ff5a7027a06a1a05ee81
Co3code <countian3@gmail.com>
2026-02-02 19:45:48 +0800

    exercise 2-8

--- exercise -2-8.php
+++ exercise -2-8.php
@@ -1,3 +1,49 @@
-<?php  
-// to be continue 
+<?php
+
+if (isset($_POST['calculate'])){
+
+    $prelim = $_POST['prelim'];
+    $midterm = $_POST['midterm'];
+    $endterm = $_POST['endterm'];
+
+
+    $finalGrade = ($prelim * 0.20 ) + ($midterm * 0.30 ) + ($endterm * 0.50 );
+
+
+    if($finalGrade >= 75){
+        $remark = "Passed";
+    }else{
+        $remark = "failed";
+    }
+      
+
+
+}
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grading</title>
+</head>
+<body>
+    <h2> Exercise 2-8 </h2>
+    <form action="" method="post">
+    Prelim Grade:<input type="number" name="prelim" required > <br><br>
+    Midterm Grade:<input type="number" name="midterm" required > <br><br>
+    Endterm Grade:<input type="number" name="endterm" required > <br><br>
+
+    <input type="submit" name="calculate" value="calculate">
+    </form>
+
+        <strong> Average Grade:</strong>
+    <?php echo isset($finalGrade) ?  number_format ($finalGrade , 2) : ''; ?> <br>
+
+    <strong>Remark:</strong>
+    <?php echo isset($remark) ? $remark : "" ;  ?> <br>
+
+</body>
+</html>
