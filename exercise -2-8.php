782c915a447f317ffb0bc27e61bae26635c00efd
Co3code <countian3@gmail.com>
2026-02-02 23:11:13 +0800

    add grade calculation logic and form

--- exercise -2-8.php
+++ exercise -2-8.php
@@ -1,25 +1,26 @@
 <?php
 
-if (isset($_POST['calculate'])){
+    if (isset($_POST['calculate'])) {
 
-    $prelim = $_POST['prelim'];
+    $prelim  = $_POST['prelim'];
     $midterm = $_POST['midterm'];
     $endterm = $_POST['endterm'];
 
+    $finalGrade = ($prelim * 0.20) + ($midterm * 0.30) + ($endterm * 0.50);
 
-    $finalGrade = ($prelim * 0.20 ) + ($midterm * 0.30 ) + ($endterm * 0.50 );
+    if ($finalGrade >= 90) {
+        $remark = "Excellent ";
+    } elseif ($finalGrade >= 80) {
+        $remark = "Good";
 
-
-    if($finalGrade >= 75){
+    } elseif ($finalGrade >= 75) {
         $remark = "Passed";
-    }else{
+    } else {
         $remark = "failed";
     }
-      
 
+    }
 
-}
-    
 ?>
 
 <!DOCTYPE html>
@@ -27,23 +28,23 @@ if (isset($_POST['calculate'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Grading</title>
+    <title>Grade Calculator</title>
 </head>
 <body>
     <h2> Exercise 2-8 </h2>
     <form action="" method="post">
-    Prelim Grade:<input type="number" name="prelim" required > <br><br>
-    Midterm Grade:<input type="number" name="midterm" required > <br><br>
-    Endterm Grade:<input type="number" name="endterm" required > <br><br>
+    Prelim Grade:<input type="number" name="prelim" required min="0" max="100 " > <br><br>
+    Midterm Grade:<input type="number" name="midterm" required  min="0" max="100 " > <br><br>
+    Endterm Grade:<input type="number" name="endterm" required   min="0" max="100" > <br><br>
 
     <input type="submit" name="calculate" value="calculate">
     </form>
 
         <strong> Average Grade:</strong>
-    <?php echo isset($finalGrade) ?  number_format ($finalGrade , 2) : ''; ?> <br>
+    <?php echo isset($finalGrade) ? number_format($finalGrade, 2) : ''; ?> <br>
 
     <strong>Remark:</strong>
-    <?php echo isset($remark) ? $remark : "" ;  ?> <br>
+    <?php echo isset($remark) ? $remark : ""; ?> <br>
 
 </body>
 </html>
