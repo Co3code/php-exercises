06f4dea1b437cd7cfffcf5acbf0adb4bb2d755f1
Co3code <countian3@gmail.com>
2026-02-06 22:52:49 +0800

    Create form with textarea and submit buttons for Exercise 3-1

--- exercise-3-1.php
+++ exercise-3-1.php
@@ -6,6 +6,33 @@
     <title>Document</title>
 </head>
 <body>
+    <h2>Exercise 3-1</h2>
+
+
+    <form action="" method="post">
+
+
+<p>
+
+
+
+
+
+<input type="submit"  name="add" value="Add to list" >
+<input type="submit"  name="clear" value="clear" >
+
+</p>
+
+
+
+
+
+
+
+<p>
+    <textarea name="list" rows="10" cols="50" style="resize: none;"><?= $list; ?></textarea>
+</p>
+    </form>
     
 </body>
 </html>
