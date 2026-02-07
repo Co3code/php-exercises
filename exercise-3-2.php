3d18a02feba7350be48a4098f382b8b587a9a0d4
Co3code <countian3@gmail.com>
2026-02-07 23:23:10 +0800

    Add salary computation page structure

--- exercise-3-2.php
+++ exercise-3-2.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Salary Computation</title>
 </head>
 <body>
+    <h2>Exercise-3-2.php</h2>
+    <form action="" method="post">
+
+    
+    </form>
     
 </body>
 </html>
