91df60a2212c2e8c14a285ab9fde75ec4e0d0308
Co3code <countian3@gmail.com>
2026-02-07 23:36:25 +0800

    Add salary computation form fields

--- exercise-3-2.php
+++ exercise-3-2.php
@@ -7,10 +7,28 @@
 </head>
 <body>
     <h2>Exercise-3-2.php</h2>
-    <form action="" method="post">
+    <form method="post">
+ No. ofDays Worked:<input type="number" name="work" required> <br><br>
 
-    
+ <label>Employee Status:</label>
+ <select name="emp_status" required >
+    <option value="">Emp Status</option>
+    <option value="regular">Regular</option>
+    <option value="probationary">Probationary</option>
+    <option value="casual">Casual</option>
+ </select>
+ <br><br>
+ <label>Civil Status:</label>
+ <select name="emp_status" required >
+    <option value="">Civil Status</option>
+    <option value="single">Single</option>
+    <option value="married">Married</option>
+    <option value="widow">Widow</option>
+ </select>
+ <br><br>
+ <input type="submit" value="Compute Salary" name="compute">
+
     </form>
-    
+
 </body>
 </html>
