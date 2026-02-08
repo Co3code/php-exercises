3c0f158e2c06c590f10faee8fc4c33f20dea3c55
Co3code <countian3@gmail.com>
2026-02-08 23:06:53 +0800

    Add name attributes to form inputs for Exercise 2-3

--- exercise-3-3.php
+++ exercise-3-3.php
@@ -13,31 +13,31 @@
 
     <strong>Proccesor</strong> <br>
     <div style="float: left; width: 10%;">
-    <input type="checkbox" name="" value="inteli7">intel i7 <br>
-    <input type="checkbox" name="" value="inteli5">intel i5 <br>
-    <input type="checkbox" name="" value="inteli3">intel i3 <br>
+    <input type="checkbox" name="proccesor" value="inteli7">intel i7 <br>
+    <input type="checkbox" name="proccesor" value="inteli5">intel i5 <br>
+    <input type="checkbox" name="proccesor" value="inteli3">intel i3 <br>
     </div>
-    <input type="checkbox" name="" value="inteli3">intel Quad Core <br>
-    <input type="checkbox" name="" value="inteli3">intel Dual Core <br>
+    <input type="checkbox" name="processor" value="inteli3">intel Quad Core <br>
+    <input type="checkbox" name="processor" value="inteli3">intel Dual Core <br>
 
 <br><br>
 
 
     <strong>Ram</strong><br>
 <div style="float: left; width: 10%;">
-    <input type="checkbox" name="" value="16GB">16 GB<br>
-    <input type="checkbox" name="" value="8GB">8 GB <br>
+    <input type="checkbox" name="ram" value="16GB">16 GB<br>
+    <input type="checkbox" name="ram" value="8GB">8 GB <br>
 
 </div>
-    <input type="checkbox" name="" value="4GB">4GB<br>
-    <input type="checkbox" name="" value="2GB">2GB<br>
+    <input type="checkbox" name="ram" value="4GB">4GB<br>
+    <input type="checkbox" name="ram" value="2GB">2GB<br>
 
     <br><br>
 
     <strong> Accesories</strong><br>
-    <input type="checkbox" name="" value="Gaming Keyboard">Gaming Keyboard<br>
+    <input type="checkbox" name="accesories" value="Gaming Keyboard">Gaming Keyboard<br>
     <input type="checkbox" name="" value="Gaming Mouse">Gaming Mouse <br>
-    <input type="checkbox" name="" value="Gaming Headset">Gaming Headset<br>
+    <input type="checkbox" name="accesories" value="Gaming Headset">Gaming Headset<br>
 
 
 <input type="submit" value="compute">
