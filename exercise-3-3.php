9f949f5f3c8dc02583c637863b1ab64512ba5b6d
Co3code <countian3@gmail.com>
2026-02-08 23:05:34 +0800

    Add computer parts selection form for Exercise 2-3

--- exercise-3-3.php
+++ exercise-3-3.php
@@ -5,8 +5,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body> 
+<body>
     <h2>Exercise 2-3</h2>
-    
+
+    <form method="post">
+
+
+    <strong>Proccesor</strong> <br>
+    <div style="float: left; width: 10%;">
+    <input type="checkbox" name="" value="inteli7">intel i7 <br>
+    <input type="checkbox" name="" value="inteli5">intel i5 <br>
+    <input type="checkbox" name="" value="inteli3">intel i3 <br>
+    </div>
+    <input type="checkbox" name="" value="inteli3">intel Quad Core <br>
+    <input type="checkbox" name="" value="inteli3">intel Dual Core <br>
+
+<br><br>
+
+
+    <strong>Ram</strong><br>
+<div style="float: left; width: 10%;">
+    <input type="checkbox" name="" value="16GB">16 GB<br>
+    <input type="checkbox" name="" value="8GB">8 GB <br>
+
+</div>
+    <input type="checkbox" name="" value="4GB">4GB<br>
+    <input type="checkbox" name="" value="2GB">2GB<br>
+
+    <br><br>
+
+    <strong> Accesories</strong><br>
+    <input type="checkbox" name="" value="Gaming Keyboard">Gaming Keyboard<br>
+    <input type="checkbox" name="" value="Gaming Mouse">Gaming Mouse <br>
+    <input type="checkbox" name="" value="Gaming Headset">Gaming Headset<br>
+
+
+<input type="submit" value="compute">
+<br><br>
+  TOTAL:<input type="text" name="total" value="0" readonly>
+
+    </form>
+
 </body>
 </html>
